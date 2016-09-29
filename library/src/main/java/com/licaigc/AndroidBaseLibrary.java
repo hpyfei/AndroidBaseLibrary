706d5fef001a2ebb5a2b9acadd9a70ee1ab135cf
walfud <walfud@aliyun.com>
2016-09-29 11:29:16 +0800

    +JPush

--- library/src/main/java/com/licaigc/AndroidBaseLibrary.java
+++ library/src/main/java/com/licaigc/AndroidBaseLibrary.java
@@ -1,17 +1,30 @@
 package com.licaigc;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.licaigc.trace.Track;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 
 /**
  * Created by walfud on 2016/7/7.
  */
-public class AndroidBaseLibrary {
+public class AndroidBaseLibrary implements Handler.Callback {
     public static final String TAG = "AndroidBaseLibrary";
 
     /**
      * Should be application context
      */
     private static Context sContext;
+
+    private static Handler sHandler;
 
     /**
      * App Id
@@ -24,15 +37,15 @@ public class AndroidBaseLibrary {
     private static int sPrimaryColor;
 
     /**
-     *
+     *  主线程调用
      * @param context
      * @return
      */
     public static final boolean initialize(Context context) {
         sContext = context.getApplicationContext();
+        sHandler = new Handler();
 
         String pkgName = AndroidBaseLibrary.getContext().getPackageName();
-
         if (false) {
             // Stub
         } else if (Constants.PKG_TALICAI.equals(pkgName)) {
@@ -52,6 +65,10 @@ public class AndroidBaseLibrary {
             sPrimaryColor = Constants.APP_PRIMARY_COLOR_UNKNOWN;
         }
 
+        // JPush
+        JPushInterface.init(context); // 初始化 JPush
+        sHandler.sendEmptyMessage(MSG_JPUSH_ALIAS);
+
         return true;
     }
 
@@ -69,5 +86,49 @@ public class AndroidBaseLibrary {
 
     public static int getPrimaryColor() {
         return sPrimaryColor;
+    }
+
+    //
+    private static final TagAliasCallback sAliasCallback = new TagAliasCallback() {
+        @Override
+        public void gotResult(int code, String alias, Set<String> tags) {
+            String logs ;
+            switch (code) {
+                case 0:
+                    logs = "Set tag and alias success";
+                    Log.i("JPush", logs);
+                    // 建议这里往 SharePreference 里写一个成功设置的状态。成功设置一次后，以后不必再次设置了。
+                    break;
+                case 6002:
+                    logs = "Failed to set alias and tags due to timeout. Try again after 60s.";
+                    Log.i("JPush", logs);
+                    // 延迟 60 秒来调用 Handler 设置别名
+                    sHandler.sendEmptyMessageDelayed(MSG_JPUSH_ALIAS, 60 * 1000);
+                    break;
+                default:
+                    logs = "Failed with errorCode = " + code;
+                    Log.i("JPush", logs);
+            }
+        }
+    };
+
+    private static final int MSG_JPUSH_ALIAS = 0x1000;
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case MSG_JPUSH_ALIAS: {
+                String alias = Track.getRefId();
+                String tagStr = PackageUtils.getVersionName().replace(".", "_");
+                Set<String> tag = new HashSet<>();
+                tag.add(tagStr);
+                JPushInterface.setAliasAndTags(sContext, alias, tag, sAliasCallback);
+                break;
+            }
+
+            default:
+                break;
+        }
+        return false;
     }
 }
