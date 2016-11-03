a0d704aa26a82cc6b51fc95f656f73556dc961f5
walfud <walfud@aliyun.com>
2016-11-03 18:17:03 +0800

    remove deprecated

--- library/src/main/java/com/licaigc/datetime/ChainCalendar.java
+++ library/src/main/java/com/licaigc/datetime/ChainCalendar.java
@@ -197,15 +197,6 @@ public class ChainCalendar {
         return this;
     }
 
-    /**
-     *
-     * @return
-     * @deprecated use {@link #get()} instead
-     */
-    public Calendar getCalendar() {
-        return mCalendar;
-    }
-
     public Calendar get() {
         return mCalendar;
     }
